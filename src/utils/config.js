e07f09d7fcd1e2ee8d5c1d95375e1211bd4d5c15
Kai Vorma <kai.vorma@iki.fi>
2026-02-13 16:58:32 +0200

    fix lint_test #skip

--- src/utils/config.js
+++ src/utils/config.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 3003
 const MONGODB_URI = test ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 const DEBUG = test ? 0 : process.env.DEBUG || 0
 const QUIET = process.env.QUIET || false
-//console.log('Config loaded:', { NODE_ENV: process.env.NODE_ENV, test, PORT, MONGODB_URI, PORT, DEBUG, QUIET })
+console.log('Config loaded:', { NODE_ENV: process.env.NODE_ENV, test, PORT, MONGODB_URI, PORT, DEBUG, QUIET })
 
 module.exports = { MONGODB_URI, PORT, DEBUG, QUIET }
