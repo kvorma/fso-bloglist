1395232043ad8f8127611b0c01f72cad99f8e08f
Kai Vorma <kai.vorma@iki.fi>
2026-02-13 18:11:54 +0200

    changed to dotenvx, #skip

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,14 +2,15 @@
 // Ymoäristömuuttujien käsittely
 // (c) 2025 Kai Vorma
 
-require('dotenv').config()
-
+require('@dotenvx/dotenvx').config({ debug: true })
 const test = process.env.NODE_ENV === 'test'
+const prod = process.env.NODE_ENV === 'production'
+const db = test ? 'blogTest' : 'blogList'
 
 const PORT = process.env.PORT || 3003
-const MONGODB_URI = test ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
-const DEBUG = test ? 0 : process.env.DEBUG || 0
-const QUIET = process.env.QUIET || false
-console.log('Config loaded:', { NODE_ENV: process.env.NODE_ENV, test, PORT, MONGODB_URI, PORT, DEBUG, QUIET })
+const MONGODB_URI = process.env.MONGODB_URI + db
+const DEBUG = test || prod ? 0 : process.env.DEBUG || 0
+const QUIET = test || prod ? true : process.env.QUIET || false
+console.log('Config loaded:', { NODE_ENV: process.env.NODE_ENV, test, MONGODB_URI, PORT, DEBUG, QUIET })
 
 module.exports = { MONGODB_URI, PORT, DEBUG, QUIET }
